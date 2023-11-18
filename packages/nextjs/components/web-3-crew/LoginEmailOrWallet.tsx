09ffa0d767f2960cbb7d03156d1d349427b705c1
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-11-18 06:02:08 -0600

    extract back button out as its own componenet BackButton

--- packages/nextjs/components/web-3-crew/LoginEmailOrWallet.tsx
+++ packages/nextjs/components/web-3-crew/LoginEmailOrWallet.tsx
@@ -1,3 +1,4 @@
+import BackButton from "./buttons/BackButton";
 import { FormSteps } from "~~/pages/dapp";
 
 interface props {
@@ -7,19 +8,7 @@ interface props {
 const LoginEmailOrWallet = ({ updateFormState }: props) => {
   return (
     <>
-      {/* Back Button */}
-      <button onClick={() => updateFormState("start")} className="btn btn-square btn-outline absolute">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
-        </svg>
-      </button>
+      <BackButton updateFormState={updateFormState} target="start" />
       {/* Main Buttons */}
       <div className="flex items-center justify-center pt-10 space-x-5">
         <button
