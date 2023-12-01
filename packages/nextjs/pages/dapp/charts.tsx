d6337d5fdb8db61dd1a487380deea52db129b292
Spencer Schoeneman <spencer.schoeneman@gmail.com>
2023-12-01 14:22:55 -0600

    add all dashboard pages

---
+++ packages/nextjs/pages/dapp/charts.tsx
@@ -0,0 +1,19 @@
+import { ReactElement } from "react";
+import type { NextPageWithLayout } from "../_app";
+import Charts from "~~/components/dash-wind/pages/protected/Charts";
+// import { MetaHeader } from "~~/components/MetaHeader";
+import DashLayout from "~~/components/layouts/DashLayout";
+
+{
+  /* <MetaHeader /> Look into MetaHeader - should it be moved to _app.tsx ??? */
+}
+
+const DappDashboard: NextPageWithLayout = () => {
+  return <Charts />;
+};
+
+DappDashboard.getLayout = function getLayout(page: ReactElement) {
+  return <DashLayout>{page}</DashLayout>;
+};
+
+export default DappDashboard;
